d02022c2f8d8a27bb03d1b63fbd248d2384c3d28
daviavila2007 <139869720+daviavila2007@users.noreply.github.com>
2024-04-05 09:18:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+function curriculo(){
+    window.location.href = "curriculo.html"
+}
+function areasT(){
+    window.location.href = "areasT.html"
+}
+function fotos(){
+    window.location.href = "fotos.html"
+}
+function ppe(){
+    window.location.href = "ppe.html"
+}
+function certificados(){
+    window.location.href = "certificados.html"
+}
+function extras(){
+    window.location.href = "extras.html"
+}
+function anterior(){
+    window.location.href = ("https://www.canva.com/design/DAFxdUgkBQk/Nx9E6RXl0q2y3P0VKuHBew/edit?utm_content=DAFxdUgkBQk&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton");
+}
+function voltar(){
+    window.location.href = ("index.html")
+}
+function trimestre1(){
+    window.location.href = ("trimestre1.html")
+}
+function voltar2(){
+    window.location.href = ("areasT.html")
+}
+function trimestre2(){
+    window.location.href = ("trimestre2.html")
+}
+function trimestre3(){
+    window.location.href = ("trimestre3.html")
+}
+function mtm1(){
+    window.location.href = ("mtm1.html")
+}
+function voltar3(){
+    window.location.href = ("trimestre1.html")
+}
+function natureza1(){
+    window.location.href = ("natureza1.html")
+}
+function tecnico1(){
+    window.location.href = ("tecnico1.html")
+}
+function humanas1(){
+    window.location.href = ("humanas1.html")
+}
+function linguagens1(){
+    window.location.href = ("linguagens1.html")
+}
+function mtm2(){
+    window.location.href = ("mtm2.html")
+}
+function natureza2(){
+    window.location.href = ("natureza2.html")
+}
+function tecnico2(){
+    window.location.href = ("tecnico2.html")
+}
+function humanas2(){
+    window.location.href = ("humanas2.html")
+}
+function linguagens2(){
+    window.location.href = ("linguagens2.html")
+}
+function mtm3(){
+    window.location.href = ("mtm3.html")
+}
+function natureza3(){
+    window.location.href = ("natureza3.html")
+}
+function tecnico3(){
+    window.location.href = ("tecnico3.html")
+}
+function humanas3(){
+    window.location.href = ("humanas3.html")
+}
+function linguagens3(){
+    window.location.href = ("linguagens3.html")
+}
+function calchoras(){
+    window.location.href = ("calchoras.html")
+}
